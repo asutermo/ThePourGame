b88553dd372aa421661fe81d89938e1fd795a310
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2011-11-03 16:04:18 -0600

    Uploading first files

---
+++ src/org/android/pourgame/ThePourGameActivity.java
@@ -0,0 +1,13 @@
+package org.android.pourgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ThePourGameActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
