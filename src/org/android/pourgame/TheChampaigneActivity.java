4f0446f47c8b76f4c417ccd79b118d70a7462f78
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2011-11-08 16:15:30 -0700

    adding different activities (for easy screen switching)

---
+++ src/org/android/pourgame/TheChampaigneActivity.java
@@ -0,0 +1,38 @@
+package org.android.pourgame;
+
+import android.app.Activity;
+
+public class TheChampaigneActivity extends Activity implements
+		OrientationListener {
+
+	@Override
+	public void onOrientationChanged(float azimuth, float pitch, float roll) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTopUp() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onBottomUp() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onRightUp() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onLeftUp() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
