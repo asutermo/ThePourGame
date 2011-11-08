14293342deb71b87979dfc27e92524c808ffa2cf
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2011-11-08 16:52:37 -0700

    added xml files, activities to manifest

--- gen/org/android/pourgame/R.java
+++ gen/org/android/pourgame/R.java
@@ -14,7 +14,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int beer=0x7f030000;
+        public static final int champaigne=0x7f030001;
+        public static final int gps=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int soda=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
