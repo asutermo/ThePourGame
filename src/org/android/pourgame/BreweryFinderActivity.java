cab23b2ebfa1098d1b7f6223f7fab8a669e542bb
unknown <Mike@.(none)>
2011-11-08 20:13:15 -0700

    Added more functionality to the Brewery finder

--- src/org/android/pourgame/BreweryFinderActivity.java
+++ src/org/android/pourgame/BreweryFinderActivity.java
@@ -3,8 +3,15 @@ package org.android.pourgame;
 import com.google.android.maps.MapActivity;
 
 import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import com.google.android.maps.*;
+import android.widget.Toast;
+
+import com.google.android.maps.MapView;
+import com.google.android.maps.MapController;
+import com.google.android.maps.GeoPoint;
 
 public class BreweryFinderActivity extends MapActivity implements
 		OrientationListener {
@@ -12,6 +19,8 @@ public class BreweryFinderActivity extends MapActivity implements
 	private static Context CONTEXT;
 	private MapView mapView;
 	private MapController mapController;
+	private LocationManager locationManager;
+	private LocationListener locationListener;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,53 @@ public class BreweryFinderActivity extends MapActivity implements
         
         mapController = mapView.getController();
         mapController.setZoom(16);
+        
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        
+        locationListener = new GPSLocationListener();
+        
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+        		0, 0, locationListener);
+    }
+    
+    private class GPSLocationListener implements LocationListener
+    {
+
+		@Override
+		public void onLocationChanged(Location location) {
+			if(location != null)
+			{
+				GeoPoint point = new GeoPoint(
+						(int)(location.getLatitude()*1E6),
+						(int)(location.getLongitude()*1E6));
+				Toast.makeText(CONTEXT, "LAT: "+location.getLatitude() +
+						"LONG: " + location.getLongitude(), Toast.LENGTH_LONG).show();
+				
+				mapController.animateTo(point);
+				mapController.setZoom(16);
+				mapView.invalidate();
+			}
+			
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			// TODO Auto-generated method stub
+			
+		}
+    	
     }
 
 
