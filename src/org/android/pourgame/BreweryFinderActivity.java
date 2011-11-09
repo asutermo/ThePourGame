a08106241d202ea5ec3a96ccb0cc3c3d43864b62
= <=>
2011-11-08 17:04:08 -0700

    Changed the Manifest to reflect the addition of the Brewery Finder and added oncreate and destroy methods for the breweryfinderactivity.java

--- src/org/android/pourgame/BreweryFinderActivity.java
+++ src/org/android/pourgame/BreweryFinderActivity.java
@@ -1,9 +1,37 @@
 package org.android.pourgame;
 
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
 
 public class BreweryFinderActivity extends Activity implements
 		OrientationListener {
+	
+	private static Context CONTEXT;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        CONTEXT = this;
+    }
+
+
+
+    protected void onResume() {
+        super.onResume();
+        if (OrientationManager.isSupported()) {
+            OrientationManager.startListening(this);
+        }
+    }
+ 
+    protected void onDestroy() {
+        super.onDestroy();
+        if (OrientationManager.isListening()) {
+            OrientationManager.stopListening();
+        }
+ 
+    }
 
 	@Override
 	public void onBottomUp() {
@@ -34,5 +62,8 @@ public class BreweryFinderActivity extends Activity implements
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	public static Context getContext() {
+		return CONTEXT;
+	}
 }
