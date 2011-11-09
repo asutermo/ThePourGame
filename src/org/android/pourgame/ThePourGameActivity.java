d608624e05d2edfd29d3b491273d574d030d1374
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2011-11-08 17:43:00 -0700

    added gesture methods

--- src/org/android/pourgame/ThePourGameActivity.java
+++ src/org/android/pourgame/ThePourGameActivity.java
@@ -3,18 +3,27 @@ package org.android.pourgame;
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Context;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.MotionEvent;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ThePourGameActivity extends Activity implements OrientationListener {
+public class ThePourGameActivity extends Activity implements OrientationListener, OnGestureListener {
     /** Called when the activity is first created. */
 	private static Context CONTEXT;
+	private static final int SWIPE_MIN = 120;
+	private static final int SWIPE_MAX_OFF = 250;
+	private static final int SWIPE_THRESH_VEL = 200;
+	private GestureDetector gestureDetector;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         CONTEXT = this;
+        gestureDetector = new GestureDetector(this, this);
+        
     }
 
 
@@ -59,5 +68,55 @@ public class ThePourGameActivity extends Activity implements OrientationListener
 
 	public static Context getContext() {
 		return CONTEXT;
+	}
+
+
+
+	@Override
+	public boolean onDown(MotionEvent arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+
+	@Override
+	public boolean onFling(MotionEvent arg0, MotionEvent arg1, float arg2,
+			float arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+
+	@Override
+	public void onLongPress(MotionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public boolean onScroll(MotionEvent arg0, MotionEvent arg1, float arg2,
+			float arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+
+	@Override
+	public void onShowPress(MotionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent arg0) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
