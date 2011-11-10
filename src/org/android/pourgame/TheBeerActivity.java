0695212b1661c8ae064c34fd86e139456f91b6e2
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2011-11-10 14:31:52 -0700

    added previous intent to move to game

---
+++ src/org/android/pourgame/TheBeerActivity.java
@@ -0,0 +1,53 @@
+package org.android.pourgame;
+
+import android.app.Activity;
+
+public class TheBeerActivity extends Activity implements
+OrientationListener {
+
+/* (non-Javadoc)
+* @see org.android.pourgame.OrientationListener#onOrientationChanged(float, float, float)
+*/
+@Override
+public void onOrientationChanged(float azimuth, float pitch, float roll) {
+// TODO Auto-generated method stub
+
+}
+
+/* (non-Javadoc)
+* @see org.android.pourgame.OrientationListener#onTopUp()
+*/
+@Override
+public void onTopUp() {
+// TODO Auto-generated method stub
+
+}
+
+/* (non-Javadoc)
+* @see org.android.pourgame.OrientationListener#onBottomUp()
+*/
+@Override
+public void onBottomUp() {
+// TODO Auto-generated method stub
+
+}
+
+/* (non-Javadoc)
+* @see org.android.pourgame.OrientationListener#onRightUp()
+*/
+@Override
+public void onRightUp() {
+// TODO Auto-generated method stub
+
+}
+
+/* (non-Javadoc)
+* @see org.android.pourgame.OrientationListener#onLeftUp()
+*/
+@Override
+public void onLeftUp() {
+// TODO Auto-generated method stub
+
+}
+
+}
