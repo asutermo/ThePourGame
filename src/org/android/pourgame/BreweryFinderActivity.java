08698f5a9513667345a561583117083fd81348f1
Mike Quinlan <mquinlan@mines.edu>
2011-11-14 23:49:47 -0700

    Added some more maps API stuff

--- src/org/android/pourgame/BreweryFinderActivity.java
+++ src/org/android/pourgame/BreweryFinderActivity.java
@@ -1,14 +1,25 @@
 package org.android.pourgame;
 
+import java.io.IOException;
+import java.util.List;
+
 import com.google.android.maps.MapActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -17,9 +28,10 @@ import android.widget.Toast;
 import com.google.android.maps.MapView;
 import com.google.android.maps.MapController;
 import com.google.android.maps.GeoPoint;
+import com.google.android.maps.Overlay;
 
 public class BreweryFinderActivity extends MapActivity{
-	
+	public static final String TAG = "BreweryFinderActivity";
 	private static Context CONTEXT;
 	private MapView mapView;
 	private MapController mapController;
@@ -105,9 +117,37 @@ public class BreweryFinderActivity extends MapActivity{
 				Toast.makeText(CONTEXT, "LAT: "+location.getLatitude() +
 						"\nLONG: " + location.getLongitude(), Toast.LENGTH_SHORT).show();
 				//Zooms before moving to your current location
-				mapController.setZoom(8);
+				mapController.setZoom(16);
 				//Animates the map to your current location
 				mapController.animateTo(point);
+				
+				MapOverlay mapOverlay = new MapOverlay();
+				mapOverlay.setLocationPoint(point);
+				List<Overlay> overlayList = mapView.getOverlays();
+				overlayList.clear();
+				overlayList.add(mapOverlay);
+				
+				Geocoder geoCoder = new Geocoder(getBaseContext());
+				List<Address> nearestBreweries;
+				
+				try {
+					nearestBreweries = geoCoder.getFromLocationName("Liquor", 10);
+					if(nearestBreweries.size() > 0){
+						//TODO: I don't know if this is the right way to go about this
+						point = new GeoPoint((int)(nearestBreweries.get(0).getLatitude()*1E6), (int)(nearestBreweries.get(0).getLongitude()*1E6));
+						mapController.animateTo(point);
+						mapOverlay = new MapOverlay();
+						mapOverlay.setLocationPoint(point);
+						overlayList.add(mapOverlay);
+					}else{
+						Log.i(TAG, "Could not find breweries for some reason");
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				
 				//Redraws the scene. I have no idea why this is here but I found it in a tutorial
 				mapView.invalidate();
 			}
@@ -133,10 +173,36 @@ public class BreweryFinderActivity extends MapActivity{
     	
     }
     
+    private class MapOverlay extends Overlay {
+    	private GeoPoint locationPoint;
+
+		public GeoPoint getLocationPoint() {
+			return locationPoint;
+		}
+
+		public void setLocationPoint(GeoPoint locationPoint) {
+			this.locationPoint = locationPoint;
+		}
+		
+		@Override
+		public boolean draw(Canvas canvas, MapView mv, boolean shadow, long when)
+		{
+			super.draw(canvas, mv, shadow);
+			Point screenPxs = new Point();
+			
+			mv.getProjection().toPixels(locationPoint, screenPxs);
+			
+			Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.pin);
+			canvas.drawBitmap(bmp, screenPxs.x, screenPxs.y-12, null);
+			
+			return true;
+		}
+    }
+    
     @Override
     public void onConfigurationChanged(Configuration con) {
       super.onConfigurationChanged(con);
-      setContentView(R.layout.brewery_finder);
+      //Do Nothing. This makes sure the layout does not reload on screen rotations.
     }
 
 
